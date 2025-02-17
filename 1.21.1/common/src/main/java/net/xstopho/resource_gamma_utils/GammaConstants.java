2af0ae870f2da76c1147fb43a71020a89a0b7b7f
Stein-N <nico-stein-98@outlook.de>
2025-02-17 13:47:08 +0100

    adding resource config api as dependency

--- 1.21.1/common/src/main/java/net/xstopho/resource_gamma_utils/GammaConstants.java
+++ 1.21.1/common/src/main/java/net/xstopho/resource_gamma_utils/GammaConstants.java
@@ -7,4 +7,6 @@ public class GammaConstants {
     public static final String MOD_ID = "resource_gamma_utils";
     public static final String MOD_NAME = "Resource Gamma Utils";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    public static final String NAMESPACE = "rcgammautils";
 }
