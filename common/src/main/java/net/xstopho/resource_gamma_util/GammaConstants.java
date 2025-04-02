bff1d942086d13b9f1973f7a2840765a518249eb
Nico Stein <nico.stein@vit-bund.de>
2025-04-02 08:52:21 +0200

    why is the vanilla lib not available

--- common/src/main/java/net/xstopho/resource_gamma_util/GammaConstants.java
+++ common/src/main/java/net/xstopho/resource_gamma_util/GammaConstants.java
@@ -1,12 +1,17 @@
 package net.xstopho.resource_gamma_util;
 
+import net.minecraft.client.KeyMapping;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GammaConstants {
-    public static final String MOD_ID = "resource_gamma_utils";
-    public static final String MOD_NAME = "Resource Gamma Utils";
+    public static final String MOD_ID = "resource_gamma_util";
+    public static final String MOD_NAME = "Resource Gamma Util";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    private static final String CATEGORY = "resource_gamma_util.key.category";
+    public static final KeyMapping TOGGLE = new KeyMapping(MOD_ID + ".key.toggle", GLFW.GLFW_KEY_F, CATEGORY);
 
     // KeyMapping
     // Translation
