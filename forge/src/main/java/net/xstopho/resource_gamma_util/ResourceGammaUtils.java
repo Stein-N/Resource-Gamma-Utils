4f60ec7ae285e4e7cd73228bfe6c1d3279cde40b
Stein-N <nico-stein-98@sbvl.de>
2025-12-11 16:31:48 +0100

    push FG7 module

---
+++ forge/src/main/java/net/xstopho/resource_gamma_util/ResourceGammaUtils.java
@@ -0,0 +1,9 @@
+package net.xstopho.resource_gamma_util;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GammaConstants.MOD_ID)
+public class ResourceGammaUtils {
+
+    public ResourceGammaUtils() {}
+}
