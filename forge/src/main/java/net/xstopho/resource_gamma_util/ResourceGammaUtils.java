0fd0a0f93bfd297b4dd2e6fa0e7d3345c90941f7
Stein-N <nico-stein-98@sbvl.de>
2025-12-11 17:37:56 +0100

    updating forge

--- forge/src/main/java/net/xstopho/resource_gamma_util/ResourceGammaUtils.java
+++ forge/src/main/java/net/xstopho/resource_gamma_util/ResourceGammaUtils.java
@@ -1,9 +1,28 @@
 package net.xstopho.resource_gamma_util;
 
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(GammaConstants.MOD_ID)
+@Mod("resource_gamma_util")
 public class ResourceGammaUtils {
 
     public ResourceGammaUtils() {}
+
+    @Mod.EventBusSubscriber(modid = GammaConstants.MOD_ID, value = Dist.CLIENT)
+    public static class ForgeClientHandler {
+
+        @SubscribeEvent
+        public static void registerKeyMapping(RegisterKeyMappingsEvent event) {
+            event.register(GammaConstants.TOGGLE);
+        }
+
+        @SubscribeEvent
+        public static void registerClientEvent(TickEvent.ClientTickEvent.Post event) {
+            GammaConstants.useHotkey(Minecraft.getInstance());
+        }
+    }
 }
