690e5fa5034113f09d8958fafc9688df5aa3b12c
Stein-N <nico-stein-98@sbvl.de>
2025-12-15 19:38:37 +0100

    final clean up

--- common/src/main/java/net/xstopho/resource_gamma_util/GammaConstants.java
+++ common/src/main/java/net/xstopho/resource_gamma_util/GammaConstants.java
@@ -8,13 +8,9 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.xstopho.resource_gamma_util.service.GammaService;
 import org.lwjgl.glfw.GLFW;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class GammaConstants {
     public static final String MOD_ID = "resource_gamma_util";
-    public static final String MOD_NAME = "Resource Gamma Util";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
     private static final KeyMapping.Category CATEGORY = new KeyMapping.Category(Identifier.fromNamespaceAndPath(MOD_ID, "main"));
     public static final KeyMapping TOGGLE = new KeyMapping(MOD_ID + ".key.toggle", GLFW.GLFW_KEY_H, CATEGORY);
@@ -23,18 +19,19 @@ public class GammaConstants {
         boolean shader = GammaService.isShaderActive();
 
         if (TOGGLE.consumeClick()) {
-            if (!shader) client.options.gamma().set(1500.00);
-            else {
-                LocalPlayer p = client.player;
-                if (p == null) return;
-
-                if (p.hasEffect(MobEffects.NIGHT_VISION)) {
-                    p.removeEffect(MobEffects.NIGHT_VISION);
-                } else {
-                    MobEffectInstance effect = new MobEffectInstance(MobEffects.NIGHT_VISION, -1, 10);
-                    p.addEffect(effect);
-                }
-            }
+            updateVision(client, shader);
         }
+    }
+
+    private static void updateVision(Minecraft client, boolean shader) {
+        if (!shader || client.options.gamma().get() == 1500) {
+            client.options.gamma().set(1500.0);
+        }
+
+        if (client.player == null) return;
+        LocalPlayer p = client.player;
+
+        if (p.hasEffect(MobEffects.NIGHT_VISION)) p.removeEffect(MobEffects.NIGHT_VISION);
+        else if (shader) p.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, -1));
     }
 }
