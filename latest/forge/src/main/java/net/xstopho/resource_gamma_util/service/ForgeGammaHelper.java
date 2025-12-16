ec25d9a6cacbda3d3f1f28e515b433964aeda04d
Stein-N <nico-stein-98@sbvl.de>
2025-12-16 21:57:15 +0100

    remove forge

--- latest/forge/src/main/java/net/xstopho/resource_gamma_util/service/ForgeGammaHelper.java
+++
@@ -1,8 +0,0 @@
-package net.xstopho.resource_gamma_util.service;
-
-public class ForgeGammaHelper implements IGammaHelper {
-    @Override
-    public boolean isShaderActive() {
-        return false;
-    }
-}
